c0e1eb11f63e36b45593e2ded4072db733dede36
Mark Washeim <blueprint@poetaster.de>
2021-03-17 15:13:52 +0100

    Added the initial translations

---
+++ translations/harbour-HackerNews-de.ts
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Hacker News</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HN</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ShowStory</name>
+    <message>
+        <source>Open URL in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
