ae01f06e6192b878d1a88afc3b80bad755af0b8f
Mark Washeim <blueprint@poetaster.de>
2021-06-08 09:17:56 +0200

    Added german translation back in.

--- qml/cover/CoverPage.qml
+++ qml/cover/CoverPage.qml
@@ -4,27 +4,107 @@
 
 import QtQuick 2.0
 import Sailfish.Silica 1.0
+import "../js/locations.js" as Locs
 
 CoverBackground {
     id: back
-    //width: parent.width - 2*x
+    property var debug;
+    property var now;
+    property string name;
+    property string lat;
+    property string lon;
+
+    function reload(){
+        debug = true;
+
+        if (name === "") { name="Berlin" ;}
+        if (lat === "") { lat="52.52"; }
+        if (lon ==="") { lon="13.41"  ;}
+
+        if (now == undefined) {
+            now = new Date();
+        }
+        var dYear = now.getFullYear() ;
+        var headerDate = now.toLocaleString().split(dYear)[0];
+        var headerTime = now.toLocaleString().split(dYear)[1];
+        if (debug ) console.debug(JSON.stringify(headerDate));
+        if (debug ) console.debug(JSON.stringify(headerTime));
+
+        var dDate = now.toISOString().replace(/T.*/,'') ;
+        var uri = "https://api.brightsky.dev/weather?lat=" + lat + "&lon=" + lon + "&date=" + dDate;
+
+        if (debug ) console.debug(JSON.stringify(uri)) ;
+
+        Locs.httpRequest(uri, function(doc) {
+            var response = JSON.parse(doc.responseText);
+
+            var dailyDate = new Date(response.weather[0].timestamp);
+            if (debug) console.debug(JSON.stringify(response.weather[0].timestamp));
+            var dailyRain =  Locs.dailyTotal(response.weather ,"precipitation");
+            var dailyCloud =  Locs.dailyAvg(response.weather ,"cloud_cover");
+            var dailyIcon =  Locs.mapIcon(response.weather[15].icon,dailyRain,response.weather[15].condition) ;
+            //var dailyIcon =  response.weather[15].icon) ;
+            var dailyLow =  Locs.dailyMin(response.weather,"temperature");
+            var dailyHigh =  Locs.dailyMax(response.weather,"temperature");
+            var dailyWind=  Locs.dailyAvg(response.weather ,"wind_speed");
+            var daily = {dailyDate: dailyDate, icon: dailyIcon , temperatureHigh:  dailyHigh, temperatureLow: dailyLow,
+                totalRain: dailyRain, cloud_cover:dailyCloud, wind_speed:dailyWind};
+
+            console.debug(JSON.stringify(daily));
+        });
+    }
+
+    function updateWeatherModel(){
+        listModel.clear();
+        var modelComplete = true;
+        for (var i = 0; i < 5; i++) {
+            if (weather[i] === ""){
+                if (debug) console.debug(JSON.stringify('weather: ' + i));
+                modelComplete = false;
+            }
+        }
+        for (var j = 0; j < 5; j++) {
+            if (modelComplete === true){
+                if (debug) console.debug(JSON.stringify('index: ' + j));
+                listModel.append(weather[j]);
+            }
+        }
+    }
+
+    onStatusChanged: {
+        reload();
+    }
+
     Label {
         anchors.top: back.top
         anchors.horizontalCenter: parent.horizontalCenter
+        anchors.verticalCenter: parent.verticalCenter
         id: label
-        text: qsTr("Deutscher") + "\n"  +
-                qsTr("Wetter") + "\n" +
+        text: qsTr("Deutscher") + "\n" +
+                qsTr("Wetter")+ "\n" +
                 qsTr("Dienst");
     }
     Image {
-
-        id:hnImage
-        anchors.centerIn: parent
+        id:logoImage
+        width: 128
+        height: 128
+        anchors.horizontalCenter: parent.horizontalCenter
+        anchors.bottom: parent.bottom
+        //anchors.centerIn: parent
         source: "/usr/share/icons/hicolor/128x128/apps/harbour-dwd.png"
-        //source: Qt.resolvedUrl("harbour-dwd.png")
-        //anchors.fill: parent
 
     }
+    Image {
+        id:dwdImage
+        width: 258
+        height: 69
+        anchors.horizontalCenter: parent.horizontalCenter
+        anchors.verticalCenter: parent.verticalCenter
+        source: "../png/dwd_logo_258x69.png"
+        Component.onCompleted:reload()
+    }
+
+
 
     /*CoverActionList {
         id: coverAction
---
+++ translations/harbour-dwd-de_DE.ts
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="16"/>
+        <source>About the DWD client</source>
+        <translation type="unfinished">Über den DWD client</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="45"/>
+        <source>A simple consumer of the</source>
+        <translation type="unfinished">Ein einfacher Konsument des</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="59"/>
+        <source>of the German Weather Service</source>
+        <translation type="unfinished">Deutschen Wetter Dienstes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="84"/>
+        <source> © 2021 Mark Washeim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="98"/>
+        <source>This program is FOSS software licensed</source>
+        <translation type="unfinished">Dieses Program ist FOSS, lizensiert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="99"/>
+        <source>GNU General Public License v3.</source>
+        <translation type="unfinished">GNU General Public License v3.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="114"/>
+        <source>Icons by Erik Flowers</source>
+        <translation type="unfinished">Icons von Erik Flowers</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="15"/>
+        <source>Deutscher</source>
+        <translation type="unfinished">Deutscher</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="16"/>
+        <source>Wetter</source>
+        <translation type="unfinished">Wetter</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="17"/>
+        <source>Dienst</source>
+        <translation type="unfinished">Dienst</translation>
+    </message>
+</context>
+<context>
+    <name>DailyDetails</name>
+    <message>
+        <location filename="../qml/pages/DailyDetails.qml" line="110"/>
+        <source>About</source>
+        <translation type="unfinished">Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DailyDetails.qml" line="116"/>
+        <source>Locations</source>
+        <translation type="unfinished">Orte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DailyDetails.qml" line="122"/>
+        <source>Refresh</source>
+        <translation type="unfinished">Neu Laden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DailyDetails.qml" line="130"/>
+        <source>Next</source>
+        <translation type="unfinished">Weiter</translation>
+    </message>
+</context>
+<context>
+    <name>LocationItem</name>
+    <message>
+        <location filename="../qml/delegates/LocationItem.qml" line="45"/>
+        <source>Delete</source>
+        <translation type="unfinished">Löschen</translation>
+    </message>
+</context>
+<context>
+    <name>LocationSearchPage</name>
+    <message>
+        <location filename="../qml/pages/LocationSearchPage.qml" line="75"/>
+        <source>Choose Location</source>
+        <translation type="unfinished">Ort wählen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LocationSearchPage.qml" line="79"/>
+        <source>Search</source>
+        <translation type="unfinished">Suchen</translation>
+    </message>
+</context>
+<context>
+    <name>ManageLocations</name>
+    <message>
+        <location filename="../qml/pages/ManageLocations.qml" line="59"/>
+        <source>Stored Locations</source>
+        <translation type="unfinished">Gespeicherte Orte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManageLocations.qml" line="65"/>
+        <source>About</source>
+        <translation type="unfinished">Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManageLocations.qml" line="71"/>
+        <source>Locations</source>
+        <translation type="unfinished">Orte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ManageLocations.qml" line="77"/>
+        <source>Refresh</source>
+        <translation type="unfinished">Neu laden</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <location filename="../qml/pages/OverviewPage.qml" line="128"/>
+        <source>About</source>
+        <translation type="unfinished">Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/OverviewPage.qml" line="134"/>
+        <source>Locations</source>
+        <translation type="unfinished">Orte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/OverviewPage.qml" line="140"/>
+        <source>Refresh</source>
+        <translation type="unfinished">Neu Laden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/OverviewPage.qml" line="180"/>
+        <source>Next</source>
+        <translation type="unfinished">Weiter</translation>
+    </message>
+</context>
+<context>
+    <name>StartPage</name>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="78"/>
+        <source>Stored Locations</source>
+        <translation type="unfinished">Gespeicherte Orte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="84"/>
+        <source>About</source>
+        <translation type="unfinished">Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="90"/>
+        <source>Add Locations</source>
+        <translation type="unfinished">Ort hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StartPage.qml" line="96"/>
+        <source>Refresh</source>
+        <translation type="unfinished">Neu Laden</translation>
+    </message>
+</context>
+</TS>
